e900bb292cae78a9dd1c90edf5c6702cec1f8c05
shanraja-1911 <91017757+shanraja-1911@users.noreply.github.com>
2022-02-08 13:17:32 +0000

    Fibonacci series added

--- Algorithms/Recursion/Fibonacci.py
+++ Algorithms/Recursion/Fibonacci.py
@@ -1,3 +1,21 @@
 # Get the Fibonacci recursively
 
- 
+def Fibonacci_rec(number):
+  #print(number)
+  if number ==0:
+    return 0 
+    
+  elif number ==1:
+    return 1
+    
+  elif number ==2:
+    return 1
+    
+  elif number > 2:
+    return Fibonacci_rec(number-2)+Fibonacci_rec(number-1)
+
+
+
+if __name__=='__main__':
+
+  print(Fibonacci_rec(10))
